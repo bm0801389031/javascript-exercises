4deb667ba3dd247ed8ae3aba9f228de4601dae11
Brian Monge <bm0801389031@gmail.com>
2021-07-20 21:10:09 -0700

    complete and commited

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return 'ERROR'
+    }
+    else if (start <= end){ 
 
-};
+    let array = [];
+    for (let i = start; i <= end; i++){
+        array.push(i)
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.reduce(reducer);
+}
+
+    else { 
+        if (end <= start){ 
+
+            let array2 = [];
+            for (let i = end; i <= start; i++){
+                array2.push(i)
+            }
+            const reducer2 = (accumulator, currentValue) => accumulator + currentValue;
+            return array2.reduce(reducer2);
+    }
+}
+}
 
 module.exports = sumAll;
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,27 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let notround = (f - 32) * .5556;
+  console.log(notround)
+  if (Number.isInteger(notround)){
+    return notround
+  } else {
+  let str = notround.toFixed(1);
+  return parseFloat(str)
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let notround2 = (c * 1.8) + 32;
+  console.log(notround2)
+  if (Number.isInteger(notround2)){
+    return notround2
+  } else {
+  let str2 = notround2.toFixed(1);
+  return parseFloat(str2)
+  }
 };
+
+//let arithamtic = (73.2 * 1.8) + 32
+//console.log(arithamtic)
 
 module.exports = {
   ftoc,
