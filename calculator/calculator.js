a6ac95d432d79b7f9ac29d64c004d86364eb85a3
Brian Monge <bm0801389031@gmail.com>
2021-08-05 14:08:42 -0700

    complete challenges

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+	const total = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, 0);
+
+    return total
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  
+	const total = array.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue
+  });
+
+    return total
 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+    let rval=1;
+    for (let i = 2; i <= num; i++){
+        rval = rval * i;
+    }
+    return rval;
+
 };
 
 module.exports = {
