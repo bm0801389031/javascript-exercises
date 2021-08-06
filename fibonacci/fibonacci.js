683a5bd01863eb87014e09861b31ef708a458b65
Brian Monge <bm0801389031@gmail.com>
2021-08-05 22:02:25 -0700

    complete fibo

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+   
+    var fib = []; // Initialize array!
+    
+    fib[0] = 0;
+    fib[1] = 1;
 
+
+    if (index < 0){
+        return 'OOPS'
+    } else {
+
+    
+    for (let i = 2; i <= index; i++) {
+      // Next fibonacci number = previous + one before previous
+      // Translated to JavaScript:
+      fib[i] = fib[i - 2] + fib[i - 1];
+      //fib.push(fib[i])
+    }
+    
+    return fib.pop()
+    }
 };
 
 module.exports = fibonacci;
