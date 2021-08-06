5495bff40e125c4b0fd281beca6d5e0e2a843783
Brian Monge <bm0801389031@gmail.com>
2021-08-06 16:14:36 -0700

    complete, had to look at answers

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+return array.reduce((oldest, currentPerson) => {
+    // oldestAge = 28
+    console.log(oldest)
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+
+    return oldestAge < currentAge ? currentPerson : oldest
+    
+    })
+};
+
+const getAge = function(birth, death) {
+    console.log(death)
+    if (!death) {
+    death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 module.exports = findTheOldest;
