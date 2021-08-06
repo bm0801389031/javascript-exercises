0f62e446b80ddb453f39ddf698bea9c824846cf9
Brian Monge <bm0801389031@gmail.com>
2021-08-05 19:48:51 -0700

    complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function(string) {
+    const processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    console.log(processedString)
+    const reversed = processedString.split("").reverse().join("")
+    console.log(reversed)
+    if (processedString == reversed){
+        return true
+    } else {
+        return false
+    }
+    
+  };
 
 module.exports = palindromes;
